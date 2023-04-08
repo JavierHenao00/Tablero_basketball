7a8fdf76dac4b42c5b7e87aac7b7a2df894c3e2a
JavierHenao00 <javiercamilohv00@gmail.com>
2023-04-07 23:51:16 -0500

    Cambios iniciales

---
+++ index.js
@@ -0,0 +1,37 @@
+let Mark=document.getElementById("marcadorA")
+let MarkA=document.getElementById("marcadorH")
+
+let count=0
+let counter=0
+function sum1(){
+    count = count+1
+    Mark.textContent = count
+}
+
+function sum2(){
+    count = count+2
+    Mark.textContent = count
+}
+
+function sum3(){
+    count = count+3
+    Mark.textContent = count
+}
+
+
+
+function sum1A(){
+    counter = counter+1
+    MarkA.textContent = counter
+}
+
+function sum2A(){
+    counter = counter+2
+    MarkA.textContent = counter
+}
+
+function sum3A(){
+    counter = counter+3
+    MarkA.textContent = counter
+}
+
